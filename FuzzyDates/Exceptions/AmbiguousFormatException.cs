ff02ee9a45105aab0c779b8dd8634d80d548020b
Andrew Arnold <andrew.s.arnold@gmail.com>
2019-10-05 10:17:59 -0400

    Unit tests for upcoming date parse constructors

---
+++ FuzzyDates/Exceptions/AmbiguousFormatException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FuzzyDates.Exceptions
+{
+	public class AmbiguousFormatException : Exception
+	{
+		public AmbiguousFormatException()
+			: base("Ambiguous date string. Could not determine if DD/MM/YYYY or MM/DD/YYYY")
+		{
+		}
+	}
+}
