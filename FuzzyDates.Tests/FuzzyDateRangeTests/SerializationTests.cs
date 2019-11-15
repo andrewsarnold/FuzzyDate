525f13bad9df35f5089799178e5c348debfcb39d
Andrew S. Arnold <andrew.s.arnold@gmail.com>
2019-11-15 12:46:01 -0500

    Quick JSON serialization tests

---
+++ FuzzyDates.Tests/FuzzyDateRangeTests/SerializationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace FuzzyDates.Tests.FuzzyDateRangeTests
+{
+	[TestClass]
+	public class SerializationTests
+	{
+		[TestMethod]
+		public void ReserializeTest()
+		{
+			try
+			{
+				var original = new FuzzyDateRange(new FuzzyDate(DateTime.Today.AddDays(-5)), FuzzyDate.Today);
+				var js = JsonConvert.SerializeObject(original);
+				var reconstructed = JsonConvert.DeserializeObject<FuzzyDateRange>(js);
+				Assert.AreEqual(original, reconstructed);
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail($"Expect no exception, but got {ex.Message}");
+			}
+		}
+	}
+}
---
+++ FuzzyDates.Tests/FuzzyDateTests/SerializationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace FuzzyDates.Tests.FuzzyDateTests
+{
+	[TestClass]
+	public class SerializationTests
+	{
+		[TestMethod]
+		public void ReserializeTest()
+		{
+			try
+			{
+				var original = FuzzyDate.Today;
+				var js = JsonConvert.SerializeObject(original);
+				var reconstructed = JsonConvert.DeserializeObject<FuzzyDate>(js);
+				Assert.AreEqual(original, reconstructed);
+			}
+			catch (Exception ex)
+			{
+				Assert.Fail($"Expect no exception, but got {ex.Message}");
+			}
+		}
+	}
+}
